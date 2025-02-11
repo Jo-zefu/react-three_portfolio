5319f7b0ba27a8e262c4e2a1ebf29c8893e534bf
Jo Ze <jolinzefu@gmail.com>
2025-02-11 13:15:13 +0800

    vite.config.js changes

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: "src",
   base: "/profile/",
   resolve: {
     alias: {
       "@": "/src",
     },
-  },
-  build: {
-    outDir: "../dist",
-    sourcemap: true,
   },
   plugins: [react()],
 });
